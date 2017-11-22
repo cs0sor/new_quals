098684161ce28e821b38094f56ae5dbd539c3934
simon oram <simon@electrosoup.co.uk>
2017-11-22 17:27:06 +0000

    commented out components

--- src/components/Qualifications.js
+++ src/components/Qualifications.js
@@ -6,9 +6,9 @@ import Units from './Units'
 
 const CentreQualifications = (props) => (
 	<div>
-		<InputSubmit submitAction={props.addQualification} />
-		<QualGroups {...props} groups={props.selectedQualGroups} />
-		<DropDownSubmit options={props.units} submitAction={props.addUnitToQualification} />
+		{/* <InputSubmit submitAction={props.addQualification} /> */}
+		{/* <QualGroups {...props} groups={props.selectedQualGroups} /> */}
+		{/* <DropDownSubmit options={props.units} submitAction={props.addUnitToQualification} /> */}
 		<Units {...props} />
 	</div>
 )
